1d4631db63985fdfdd58202f634b9d840c35607a
Shehroz Ali <sshehroza13@gmail.com>
2022-07-05 14:03:22 +0500

    Created Photo @Entity class

--- src/main/java/com/example/demo/TestGcpCloudsqlApplication.java
+++ src/main/java/com/example/demo/TestGcpCloudsqlApplication.java
@@ -3,6 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @SpringBootApplication
 public class TestGcpCloudsqlApplication {
 
@@ -10,4 +14,44 @@ public class TestGcpCloudsqlApplication {
 		SpringApplication.run(TestGcpCloudsqlApplication.class, args);
 	}
 
+}
+@Entity
+@Table(name = "photos")
+class Photo {
+	@Id
+	private String id;
+	private String uri;
+	private String labels;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getLabels() {
+		return labels;
+	}
+
+	public void setLabels(String labels) {
+		this.labels = labels;
+	}
+
+	public Photo(String id, String uri, String labels) {
+		this.id = id;
+		this.uri = uri;
+		this.labels = labels;
+	}
+	public Photo() {
+	}
 }
