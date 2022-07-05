6389a11e6dd967e786ce61d3bfb246c66099758f
Shehroz Ali <sshehroza13@gmail.com>
2022-07-05 13:34:40 +0500

    first push

---
+++ src/main/java/com/example/demo/TestGcpCloudsqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGcpCloudsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGcpCloudsqlApplication.class, args);
+	}
+
+}
